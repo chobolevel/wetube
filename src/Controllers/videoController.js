9ec9afb281ffb1cce5a88aea00992a5e9d8e31d3
chobolevel <rodaka123@naver.com>
2022-10-05 23:08:57 +0900

    유튜브 클론코딩 10.05

--- src/Controllers/videoController.js
+++ src/Controllers/videoController.js
@@ -1,11 +1,36 @@
-const fakeUser = {
-  username : "Nicolas",
-  loggedIn : false,
-}
-//로그인 로그아웃 매뉴 conditional위해 만든 가짜유저
+let videos = [
+  {
+    title : "First Video",
+    rating : 5,
+    comments : 2,
+    createdAt : "2 minutes ago",
+    views : 59,
+    id : 1,
+  },
+  {
+    title : "Second Video",
+    rating : 5,
+    comments : 2,
+    createdAt : "2 minutes ago",
+    views : 59,
+    id : 2,
+  },
+  {
+    title : "Third Video",
+    rating : 5,
+    comments : 2,
+    createdAt : "2 minutes ago",
+    views : 59,
+    id : 3,
+  }
+]
 
-export const trending = (req, res) => res.render("home", {pageTitle : "Home", fakeUser, });
-export const see = (req, res) => res.render("watch");
+export const trending = (req, res) => res.render("home", {pageTitle : "Home", videos, });
+export const see = (req, res) => {
+  const id = req.params.id;
+  const video = videos[id - 1];
+  return res.render("watch", {pageTitle : `Watch ${video.title}`})
+};
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => {
   return res.send("Search");
