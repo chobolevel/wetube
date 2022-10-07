1795bd478b3abe7d3ff4de8189a9726879aebaa9
chobolevel <rodaka123@naver.com>
2022-10-07 23:59:20 +0900

    유튜브 클론 코딩 10.07

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -6,8 +6,8 @@ const videoRouter = express.Router();
 //upload가 id밑에 있으면 구분을 못하기 때문에 제일 위에서 쓰임
 //예)/videos/upload -> watch Video #/upload
 //정규식을 이용해서 숫자만 받아오기 때문에 upload는 밑으로 가도 ok
-videoRouter.route("/:id(\\d+)").get(watch);
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})").get(watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
