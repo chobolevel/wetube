2acb9c9903ccc78e9ee59305bc5dd8402d48097a
chobolevel <rodaka123@naver.com>
2022-10-12 18:23:55 +0900

    유튜브 클론 코딩 10.12

--- src/Controllers/userController.js
+++ src/Controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/User";
 import bcrypt from "bcrypt";
+import fetch from "node-fetch";
 
 export const getJoin = (req, res) => {
   return res.render("join", {
@@ -42,38 +43,110 @@ export const postJoin = async (req, res) => {
 export const edit = (req, res) => {
   res.send("Edit User");
 }
-export const remove = (req, res) => {
-  res.send("Remove Users");
-}
 //Login
 export const getLogin = (req, res) => {
-  res.render("login", {pageTitle : "Login"});
+  res.render("login", { pageTitle: "Login" });
 }
 export const postLogin = async (req, res) => {
   const pageTitle = "Login"
   const { username, password } = req.body;
-  const user = await User.findOne({username});
+  const user = await User.findOne({ username, socialOnly : false });
   //계정이 존재하는 지 확인
-  if(!user) {
+  //소셜 로그인을 한 경우 password를 이용한 로그인 차단
+  if (!user) {
     return res.status(400).render("login", {
       pageTitle,
-      errorMessage : "An account with this username does not exists.",
+      errorMessage: "An account with this username does not exists.",
     });
   }
   const ok = await bcrypt.compare(password, user.password);
-  if(!ok) {
+  if (!ok) {
+    //비밀번호가 틀린 경우
     return res.status(400).render("login", {
       pageTitle,
-      errorMessage : "Wrong Password.",
+      errorMessage: "Wrong Password.",
     });
   }
   req.session.loggedIn = true;
   req.session.user = user;
   res.redirect("/");
 }
+//로그아웃
 export const logout = (req, res) => {
-  res.send("Log out");
+  req.session.destroy();
+  return res.redirect("/");
 }
 export const see = (req, res) => {
   res.send("See User");
+}
+export const startGithubLogin = (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/authorize";
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    allow_signup: false,
+    scope: "read:user user:email",
+  }
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  return res.redirect(finalUrl);
+};
+export const finishGithubLogin = async (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/access_token";
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    client_secret: process.env.GH_SECRET,
+    code: req.query.code,
+  }
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  const tokenRequest = await (await fetch(finalUrl, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+    }
+  })).json();
+  if ("access_token" in tokenRequest) {
+    const { access_token } = tokenRequest;
+    const apiUrl = "https://api.github.com";
+    const userData = await
+      (await fetch(`${apiUrl}/user`, {
+        headers: {
+          Authorization: `token ${access_token}`,
+        }
+      })).json();
+    const emailData = await
+      (await fetch(`${apiUrl}/user/emails`, {
+        headers: {
+          Authorization: `token ${access_token}`,
+        }
+      })).json();
+    const emailObj = emailData.find(email => email.primary === true && email.verified === true);
+    if (!emailObj) {
+      //이메일을 받아오지 못한 경우 LOGIN페이지로 보냄
+      return res.redirect("/login");
+    }
+    //해당 이메일로 이미 계정이 있으면 로그인 하도록 만듦
+    let user = await User.findOne({ email: emailObj.email });
+    if (!user) {
+      //여기는 Github이메일로 생성된 아이디가 없는 경우
+      user = await User.create({
+        name: userData.name,
+        avatarUrl : userData.avatar_url,
+        email: emailObj.email,
+        username: userData.login,
+        password: "",
+        socialOnly: true,
+        location: userData.location,
+      });
+      req.session.loggedIn = true;
+      req.session.user = user;
+      return res.redirect("/");
+    }
+    req.session.loggedIn = true;
+    req.session.user = user;
+    return res.redirect("/");
+  } else {
+    //만약 토큰을 못받았다면 LOGIN화면으로 보내짐
+    res.redirect("/login");
+  }
 }
