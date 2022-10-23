d6c7d39ef9b41b45dec7ba63aa343ba227ace4dd
chobolevel <rodaka123@naver.com>
2022-10-23 12:21:58 +0900

    유튜브 클론코딩 10.23

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,10 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
-  entry: "./src/client/js/main.js",
+  entry: {
+    main: "./src/client/js/main.js",
+    videoPlayer: "./src/client/js/videoPlayer.js",
+  },
   mode: "development",
   watch: true, //계속 실행상태를 유지하기 위함
   plugins: [new MiniCssExtractPlugin({
@@ -10,7 +13,7 @@ module.exports = {
   })],
   output: {
     //확장자 js파일은 모두 아래 처리를 따름
-    filename: "js/main.js",
+    filename: "js/[name].js",
     path: path.resolve(__dirname, "assets"),
     clean: true, //시작하기 전에 폴더를 깨끗하게 비우는 옵션
   },
